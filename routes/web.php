e44c4e1561504d24eb9caf21a33bec750b65f937
Sokinkeo <73463883+Sokinkeo@users.noreply.github.com>
2021-11-15 10:55:24 +0700

    newesr

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use Illuminate\Support\Facades\File;
@@ -16,15 +17,19 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-
-
     return view('posts',[
        'posts'=> Post::all()
     ]);
 });
 
-Route::get('posts/{post}', function ($slug){
-return view('post',[
-    'post'=> Post::findOrFail($slug)
-]);
-})->where('post','[A-z_\-]+');
+Route::get('posts/{post:slug}', function (Post $post) {
+    return view('post', [
+        'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
+});
